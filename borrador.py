07e737ee642375a2b4a86bee47eb36f4497f8a7c
awacero <acerowilson@gmail.com>
2016-12-21 10:38:05 -0500

    Corregido importante error de overflow en la funcion getRMS.
    getRMS() utiliza variables numpy del tipo float64

--- borrador.py
+++ borrador.py
@@ -24,73 +24,64 @@ from obspy.clients.filesystem.sds import Client as clientArchive
 import json
 
 
-
-'''
-def principal(clienteArclink,red, estacion, localizacion, canal, diaUTC, intervalo):
-    """
-    
-    Get the data streamStat of a station, calculates its RSAM and store in a DB
-    
-    Input: a arclink client, and estation parameters
-    Get the station data streamStat
-    Call rmsBanda to fill dict datosFila for rsam_banda1,2,3,4
-    Fill rms field in dict datosFila
-    Fill station,channel,date in dict datosFila
-    Store dict datosfila in DB 
-    
-    """
-    flujo=sda.getFlujo(clienteArclink, red, estacion, localizacion, canal, diaUTC, intervalo)
-    if isinstance(flujo,streamStat.Stream):
-        if len(flujo) > 0:
-            rmsBanda(flujo)
-            datosFila['rms']=getRMS(flujo[0].data)
-            datosFila['rsam_estacion']=estacion
-            datosFila['rsam_canal']=canal
-            datosFila['rsam_fecha_proceso']=str(diaUTC.datetime)
-            logging.info( datosFila)
-            try:
-                # rsamBase.insertarFila(datosFila)
-                print(datosFila)
-            except Exception as e:
-                logging.info("Error on insertarFila(): %s" %str(e))
-        else:
-            insertarVacio(estacion,canal,diaUTC)
-            try:
-                #rsamBase.insertarFila(datosFila)
-                print(datosFila)
-            except Exception as e:
-                logging.info("Error on insertarFila() Vacia: %s" %str(e))
-
-            logging.info( "No hay datos disponibles, Insertando 0s")
-
-    else:
-        insertarVacio(estacion,canal,diaUTC)
-        logging.info( "ERROR: %s . Insertando 0s" %flujo)
-
-'''
-
 def getRMS(datosFlujo):
-    datosCuadrado=pow(datosFlujo,2)
-    sumatorioDatos=np.int64()
+    datosCuadrado=[]
+    print(datosFlujo)
+    sumatorioDatos=np.float64()
+    mediaDatos=np.float64()
+    #print(datosFlujo)
+    #print("longitud")
+    #print(len(datosFlujo))
     
+    #print(int(datosFlujo))
+    
+    
+    
+    #datosCuadrado=pow((datosFlujo),2)
+    #datosCuadrado=np.sqrt(datosFlujo)
+    #datosCuadrado=np.array(datosFlujo)**2
+    
+    #for i in range(len(datosFlujo)):
+    #    datosCuadrado.append(pow(datosFlujo[i],2) )
+    
+    '''
+    datosCuadrado=np.square(datosFlujo)
+    print('array al cuadrado igual a: ')
+    print (datosCuadrado)
+    sumatorioDatos=np.int64()
+    sumatorioDatos=0
     for datos in datosCuadrado:
         sumatorioDatos=sumatorioDatos + datos
-        
-    mediaDatos=np.float64()
+    '''
+    
+    
+    #'''
+    arrayNP64=np.array(datosFlujo,dtype='float64')
+    print(arrayNP64)
+    arrayNP64SQR=np.square(arrayNP64)
+    print(arrayNP64SQR)
+    
+    for datos in arrayNP64SQR:
+        sumatorioDatos=sumatorioDatos + datos       
+    #'''
+    
+    
     mediaDatos=sumatorioDatos/float(len(datosFlujo))
+    print("media: %s" %mediaDatos)
+
     rms=mediaDatos**(0.5)
     return rms
 
 
 def insertarVacio(estacion,canal,diaUTC):
-    datosFila['rsam_estacion']=estacion
-    datosFila['rsam_canal']=canal
-    datosFila['rsam_fecha_proceso']=str(diaUTC.datetime)
-    datosFila['rms']=0
-    datosFila['rsam_banda1']=0
-    datosFila['rsam_banda2']=0
-    datosFila['rsam_banda3']=0
-    datosFila['rsam_banda4']=0   
+    rmsData['rsam_estacion']=estacion
+    rmsData['rsam_canal']=canal
+    rmsData['rsam_fecha_proceso']=str(diaUTC.datetime)
+    rmsData['rms']=0
+    rmsData['rsam_banda1']=0
+    rmsData['rsam_banda2']=0
+    rmsData['rsam_banda3']=0
+    rmsData['rsam_banda4']=0   
         
 def fillRMS(strSta):
     
@@ -99,6 +90,7 @@ def fillRMS(strSta):
     rmsData['rsam_fecha_proceso']=str(diaUTC.datetime)
     
     rmsData['rms']=getRMS(strSta[0].data)
+
     
     dataFilt1=filter.bandpass(strSta[0].data,0.05,0.125,strSta[0].stats['sampling_rate'])
     rmsData['rsam_banda1']=getRMS(dataFilt1)
@@ -137,37 +129,67 @@ def chooseService(parSrv):
             print("Error Archive : %s -- %s" %(str(e),parSrv['archivePath']))
             return -1
 
-def readConfigFile():
+def readConfigFile(jsonFile):
     
     try:
-        with open(configurationFile) as json_data_files:
+        with open(jsonFile) as json_data_files:
             return json.load(json_data_files)
     except Exception as e:
         print("Error readConfigFile(): %s" %str(e))
         return -1
+
+def getStore(service,net,cod,loc,cha,dUTC):
+    print('Starting station: %s %s %s %s' %(net,cod,loc,cha))
+    if service=='ARCLINK':
+        try:
+            streamStat=conexion.get_waveforms(net,cod,loc,cha,dUTC,dUTC + 60,route=False,compressed=False)
+        except Exception as e:
+            print("Error getting streamStat: %s" %str(e))
+    else:
+        try:
+            streamStat=conexion.get_waveforms(net,cod,loc,cha,dUTC,dUTC + 60)
+        except Exception as e:
+            print("Error getting streamStat: %s" %str(e))
+            
+    if len(streamStat)>0:
+        print("Starting calculateRSAM()")
+        print(streamStat)
+        fillRMS(streamStat)
+    else:
+        print("insertar 0")
+        insertarVacio(sta['cod'], cha, diaUTC)  
     
+    print(rmsData)
+
     
-rmsData={'rsam_estacion':'','rsam_canal':'','rsam_fecha_proceso':'','rms':'','rsam_banda1':'','rsam_banda2':'','rsam_banda3':'','rsam_banda4':''}
 execDir=os.path.realpath(os.path.dirname(__file__))
-logging.basicConfig(filename=os.path.join(execDir,"RsamLog.log"),level=logging.DEBUG)
+logging.basicConfig(filename=os.path.join(execDir,"rsam.log"),level=logging.DEBUG)
+
 configurationFile='rsamConfiguration.json'
+stationsFile="stations.json"
+rmsData={'rsam_estacion':'','rsam_canal':'','rsam_fecha_proceso':'','rms':'','rsam_banda1':'','rsam_banda2':'','rsam_banda3':'','rsam_banda4':''}
 
 
 
-intervalo=60
-#2015.6.1, MINUTO 10, 2015-06-01T00:01:00.000000Z
+
 diaString=datetime.now().strftime('%Y-%m-%d %H:%M:00')
-diaUTC=UTCDateTime(diaString) - 300
-diaUTC=UTCDateTime('2016-12-16 19:08:00')
-logging.info( diaUTC)
-servicio='ARCLINK'
+diaUTC=UTCDateTime(diaString) - 60
+#diaUTC=UTCDateTime('2016-12-16 19:08:00')
+diaUTC=UTCDateTime('2016-12-21 12:57:00')
+
+
 REVS_BD={'net':'EC','cod':'REVS','loc':('01','02'),'cha':('BDF',)}
-REVS_HH={'net':'EC','cod':'REVS','loc':('',),'cha':('HHX','HHE')}
-BREF={'net':'EC','cod':'BREF','loc':('',),'cha':('BHZ',)}
-stations=(BREF,)
+REVS_HH={'net':'EC','cod':'REVX','loc':('',),'cha':('HHX','HHE')}
+BREF_BH={'net':'EC','cod':'BREF','loc':('',),'cha':('BHZ',)}
+CONE_SH={'net':'EC','cod':'CONE','loc':('',),'cha':('SHZ',)}
 
 
-serviceParam=readConfigFile()
+servicio='ARCHIVE'
+stations=(REVS_BD,BREF_BH,CONE_SH)
+
+
+
+serviceParam=readConfigFile(configurationFile)
 if  serviceParam == -1:
     print("Error reading configuration file: %s" %configurationFile)
     exit(-1)
@@ -177,12 +199,37 @@ if conexion == -1:
     print("Error connecting service: %s" %servicio)
     exit(-1)
 
-#Parametros lista diccionario estacion,conexion,tipoServicio, dia-hora, intervalo
+stations=readConfigFile(stationsFile)
+
+for stname,sta in stations.iteritems():
+    print stname
+    for loc in sta['loc']:
+        for cha in sta['cha']:
+            print(sta['net'],sta['cod'],loc,cha)
+            getStore(servicio,sta['net'],sta['cod'],loc,cha,diaUTC)
+
+
+
+
+
+'''
+for sta in stations:
+    for loc in sta['loc']:
+        for cha in sta['cha']:
+                getStore(servicio,sta['net'],sta['cod'],loc,cha,diaUTC)
+
+'''    
+
+#SERVICIO, DICCIONARIO, HORA
+
+##Parametros lista de diccionario estacion,conexion,tipoServicio, dia-hora, intervalo
+'''
 
 for sta in stations:
     for loc in sta['loc']:
         for cha in sta['cha']:
             print "%s, %s,%s, %s" %(sta['net'],sta['cod'],loc,cha)
+            
             
             if servicio=='ARCLINK':
                 try:
@@ -202,11 +249,12 @@ for sta in stations:
                     continue
             if len(streamStat)>0:
                 print("Starting calculateRSAM()")
-                print fillRMS(streamStat)
-                print rmsData
+                fillRMS(streamStat)
+                
             else:
                 print("insertar 0")
                 insertarVacio(sta['cod'], cha, diaUTC)
-                
+            
+            print rmsData
 
-                
+'''          
